c39fc93d75b30c9d7de5c7e974a0180f8d27fff5
jordadadams <jordanadams61791@gmail.com>
2023-04-10 19:09:30 -0500

    cors install

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import videoRoutes from './routes/videoRoutes.js';
 
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
